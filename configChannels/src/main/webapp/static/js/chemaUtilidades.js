54710845f6edcbe7bb5cc0f4a290aaa295a2156e
Andres Puga Lugo <apuga@izzi.mx>
2018-03-09 22:36:01 -0600

    Update

--- configChannels/src/main/webapp/static/js/chemaUtilidades.js
+++ configChannels/src/main/webapp/static/js/chemaUtilidades.js
@@ -24,4 +24,80 @@ function ObjetoAjax(){
 	this.textoEstado = m_textoEstado;
 	
 	// Funciones para la implementación de los métodos
+	
+	function m_enviar(url, method, funcionRetorno, objForm){
+		var dataSend = null; 
+		// Si el metodo es POST, entonces serializa los datos del form
+		if(method.toLowerCase() == "post" && objForm != null){
+			dataSend = obtenerDatos(objForm);
+		}
+		// Si el metodo es GET, serializa los datos del form y los anexa a la URL
+		else if(method.toLowerCase() == "get" && objForm != null){
+			var dataUrl = "";
+			dataUrl = obtenerDatos(objForm);
+			if(url.indexOf("?") == -1){
+				url += "?"+dataUrl;
+			} else{
+				url += "&"+dataUrl;
+			}
+		}
+	}
+	
+	// Realiza la peticion siempre en modo Asincrono
+	xhr.open(method, url, true);
+	xhr.onreadystatechange = function(){
+		// Si la respuesta esta disponible, se ejecuta la funcion de retorno...
+		if(xhr.readyState == 4){
+			eval(funcionRetorno+"("+")");
+		}
+	};
+	
+	// Establece el encabezado apropiado para el envio de datos del form
+	if(objForm != null){
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	}
+	
+	// Si es POST => dataSend va lleno, GET => dataSend = null
+	xhr.send(dataSend);
+	
+	// Serializa los parametros del form
+	function obtenerDatos(objForm){
+        var controles=objForm.elements;
+        var datos=new Array();
+        var cad="";
+        for(var i=0;i<controles.length;i++){
+             cad=encodeURIComponent(controles[i].name)+"=";
+             cad+=encodeURIComponent(controles[i].value);
+             datos.push(cad);
+        }
+        //se forma la cadena con el método join() del array
+        //para evitar múltiples concatenaciones
+        cad=datos.join("&");
+        return cad;
+     }
+	
+	// Envio de la respuesta
+	function m_texto(){
+		return xhr.responseText;
+	}
+	function m_XML(){
+		return xhr.responseXML;
+	}
+	function m_encabezados(){
+		return xhr.getAllResponseHeaders();
+	}
+	
+	// Estado de la respuesta
+	function m_estado(){
+		return xhr.status;
+	}
+	function m_textoEstado(){
+		return xhr.statusText;
+	}
 }
+
+
+
+
+
+
